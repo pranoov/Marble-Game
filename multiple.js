98afe6ab367d5d9199dafc32cd4aa0706715981d
pgsmart <101372312+pgsmart@users.noreply.github.com>
2023-10-15 17:49:19 -0500

    Add files via upload

---
+++ multiple.js
@@ -0,0 +1,192 @@
+let marbles1 = Math.floor(Math.random() * 14) + 2
+let marbles2 = Math.floor(Math.random() * 14) + 2
+let marbles3 = Math.floor(Math.random() * 14) + 2
+let turn;
+let gameOver = false;
+let name = localStorage.getItem('name')
+let compScore = 0;
+let playerScore = 0;
+
+function start(){
+    if(localStorage.getItem('compScore') == undefined){
+        localStorage.setItem('compScore',compScore)
+    }else{
+        compScore = localStorage.getItem('compScore')
+        compScore = parseInt(compScore) 
+    }
+    if(localStorage.getItem('playerScore') == undefined){
+        localStorage.setItem('playerScore',playerScore)
+    }else{
+        playerScore = localStorage.getItem('playerScore')
+        playerScore = parseInt(playerScore)
+    }
+    document.getElementById('playerStuff').hidden = true
+    document.getElementById('next').hidden = true
+    document.getElementById('back').hidden = true
+    displayMarbles()
+    document.getElementById('m1').innerHTML = 'Marble Set 1: ' + marbles1 + ' Marbles'
+    document.getElementById('m2').innerHTML = 'Marble Set 2: ' + marbles2 + ' Marbles'
+    document.getElementById('m3').innerHTML = 'Marble Set 3: ' + marbles3 + ' Marbles'
+    turn = Math.floor(Math.random()*2 ) + 1
+    game()
+}
+
+function game(){
+    document.getElementById('playerStuff').hidden = true
+    document.getElementById('next').hidden = true
+    if(turn == 1 && gameOver == false){
+        playerTurn()
+    }else if(!gameOver){
+        computerTurn()
+    }
+}
+
+function playerTurn(){
+ 
+    document.getElementById('playerStuff').hidden = false;
+    document.getElementById('display').innerHTML = name + ' choose what pile you want to take marbles from.'
+    updateMarbles()
+}
+
+function updateMarbles(){
+    let pile = document.getElementById('piles').value
+    let e = document.getElementById('marbles')
+    let count = 0;
+    while(e.firstChild){
+        e.removeChild(e.lastChild)
+    }
+    console.log(pile)
+    if(pile == 1){
+        count = marbles1
+    }else if(pile == 2){
+        count = marbles2
+    }else{
+        count = marbles3
+    }
+    if(count == 1){
+        var option = new Option
+            option.value = 1;
+            option.innerHTML = 1;
+            document.getElementById('marbles').appendChild(option)
+    }else{
+        for(var i = 1; i < count; i++){
+            var option = new Option
+            option.value = i;
+            option.innerHTML = i;
+            document.getElementById('marbles').appendChild(option)
+        }
+    }
+}
+
+function computerTurn(){
+    document.getElementById('playerStuff').hidden = true
+    var pile = Math.floor(Math.random() * 3) + 1
+    var maxAmount = 0;
+    if(pile === 1) maxAmount = marbles1 -1
+    if(pile === 2) maxAmount = marbles2 -1
+    if(pile === 3) maxAmount = marbles3 -1
+    var marblesTaken = Math.floor(Math.random() * maxAmount) + 1
+    document.getElementById('display').innerHTML = "The Computer Is Thinking " 
+    setTimeout(()=>{
+        document.getElementById('next').hidden = false
+        document.getElementById('display').innerHTML = "The Computer Took " + marblesTaken + ' Marbles From Pile ' + pile 
+        switch(pile){
+            case 1: marbles1 -= marblesTaken
+                break;
+            case 2: marbles2 -= marblesTaken
+                break;
+            case 3: marbles3 -= marblesTaken
+                break;
+        }
+        document.getElementById('m1').innerHTML = 'Marble Set 1: ' + marbles1 + ' Marbles'
+        document.getElementById('m2').innerHTML = 'Marble Set 2: ' + marbles2 + ' Marbles'
+        document.getElementById('m3').innerHTML = 'Marble Set 3: ' + marbles3 + ' Marbles'
+        displayMarbles()
+
+    },3000)
+    
+}
+
+function submit(){
+    let pile = document.getElementById('piles').value
+    let marbles = document.getElementById('marbles').value
+    switch(pile){
+        case "1": marbles1 -= marbles
+            break;
+        case "2": marbles2 -= marbles
+            break;
+        case "3": marbles3 -= marbles
+            break;
+    }
+    document.getElementById('m1').innerHTML = 'Marble Set 1: ' + marbles1 + ' Marbles'
+    document.getElementById('m2').innerHTML = 'Marble Set 2: ' + marbles2 + ' Marbles'
+    document.getElementById('m3').innerHTML = 'Marble Set 3: ' + marbles3 + ' Marbles'
+    if(marbles1 == 0 || marbles2 == 0 || marbles3 == 0){
+        gameOver = true;
+        playerScore += 1;
+        localStorage.setItem('playerScore',playerScore)
+        document.getElementById('display').innerHTML = 'Player Won! Computer Lost.'
+        document.getElementById('back').hidden = false
+    }
+    turn = 2;
+    displayMarbles()
+    game()
+}
+
+function next(){
+    if(marbles1 == 0 || marbles2 == 0 || marbles3 == 0){
+        gameOver = true;
+        compScore += 1;
+        localStorage.setItem('compScore',compScore)
+        document.getElementById('display').innerHTML = 'Computer Won! Player Lost.'
+        document.getElementById('back').hidden = false
+    }
+    turn = 1;
+    game()
+}
+
+
+function displayMarbles(){
+        for(var i = 0; i < 4; i++){
+            switch(i){
+                case 1:             
+                e = document.getElementById('set1')
+                while(e.firstChild){
+                    e.removeChild(e.lastChild)
+                }
+                break;
+                case 2: 
+                e = document.getElementById('set2')
+                while(e.firstChild){
+                    e.removeChild(e.lastChild)
+                }
+                break;
+                case 3: 
+                e = document.getElementById('set3')
+                while(e.firstChild){
+                    e.removeChild(e.lastChild)
+                }
+                break;
+            }
+
+        }
+        for(var i = 0; i < marbles1; i++){
+            var image = new Image(width=60)
+            image.src = './assets/marble1.jpeg'
+            document.getElementById('set1').appendChild(image)
+        }
+        for(var i = 0; i < marbles2; i++){
+            var image = new Image(width=60)
+            image.src = './assets/marble1.jpeg'
+            document.getElementById('set2').appendChild(image)
+        }
+        for(var i = 0; i < marbles3; i++){
+            var image = new Image(width=60)
+            image.src = './assets/marble1.jpeg'
+            document.getElementById('set3').appendChild(image)
+        }
+}
+
+function home(){
+    window.location.href = 'index.html'
+}
---
+++ script.js
@@ -0,0 +1,232 @@
+let name;
+let turn;
+let marbleCount;
+let gameOver = false;
+let comFirst = false;
+let playerScore = 0;
+let compScore = 0;
+let answered = false;
+let mode;
+let amount = 0;
+
+function initialize(){
+    if(localStorage.getItem('compScore') == undefined){
+        localStorage.setItem('compScore',compScore)
+    }else{
+        compScore = localStorage.getItem('compScore')
+        compScore = parseInt(compScore)
+    }
+    if(localStorage.getItem('playerScore') == undefined){
+        localStorage.setItem('playerScore',playerScore)
+    }else{
+        playerScore = localStorage.getItem('playerScore')
+        playerScore = parseInt(playerScore)
+    }
+    document.getElementById('playerScore').innerHTML = 'Player Wins: ' + playerScore
+    document.getElementById('compScore').innerHTML = 'Computer Wins: ' + compScore
+}
+
+function chooseAmount(val){
+    if(val == 'little'){
+        document.getElementById('modeDiv').hidden = true;
+        document.getElementById('submit').disabled = false
+        document.getElementById('little').style.backgroundColor = '#37d461'
+        document.getElementById('more').style.backgroundColor = ''
+        document.getElementById('lot').style.backgroundColor = ''
+        amount = 1;
+    }else if(val == 'more'){
+        document.getElementById('modeDiv').hidden = false;
+        document.getElementById('submit').disabled = true;
+        document.getElementById('little').style.backgroundColor = ''
+        document.getElementById('more').style.backgroundColor = '#37d461'
+        document.getElementById('lot').style.backgroundColor = ''
+        amount = 2;
+    }else if(val == 'lot'){
+        document.getElementById('modeDiv').hidden = true;
+        document.getElementById('submit').disabled = false
+        document.getElementById('lot').style.backgroundColor = '#37d461'
+        document.getElementById('more').style.backgroundColor = ''
+        document.getElementById('little').style.backgroundColor = ''
+        amount = 3;
+    }
+}
+function chooseMode(val){
+    if(val == 'easy'){
+        document.getElementById('easy').style.backgroundColor = '#37d461'
+        document.getElementById('hard').style.backgroundColor = ''
+    }else{
+        document.getElementById('easy').style.backgroundColor = ''
+        document.getElementById('hard').style.backgroundColor = '#f5625b'
+    }
+    mode = val
+    document.getElementById('submit').disabled = false
+}
+function start(){
+    name = document.getElementById('name').value
+    if(amount == 3){
+        localStorage.setItem('name',name)
+        window.location.href = 'multiple.html'
+    }else{
+        game()
+    }
+}
+
+function game(){
+    gameOver = false;
+    turn = Math.floor(Math.random()*2 ) + 1
+    if(turn === 2) comFirst = true
+    if(amount == 1){
+        marbleCount = Math.floor(Math.random()*10 ) + 10
+    }else if(amount == 2){
+        marbleCount = Math.floor(Math.random()*20 ) + 10
+    }
+    
+    while(!gameOver){
+        if(turn === 1){
+            console.log(turn)
+            playerTurn()
+        }else{
+            computerTurn()
+        }
+   }
+    document.getElementById('playerScore').innerHTML = 'Player Wins: ' + playerScore
+    document.getElementById('compScore').innerHTML = 'Computer Wins: ' + compScore
+}
+
+function playerTurn(){
+        let answer = 0;
+        
+        if(amount == 1){
+            while(answer != 1 && answer != 2 || answer === undefined || answer === 0){
+                if(answered){
+                    if(marbleCount != 1){
+                        answer = prompt(name + ', YOU NEED TO CHOOSE EITHER 1 OR 2. THERE ARE '+ marbleCount + ' MARBLES LEFT. PLS TRY AGAIN!')
+                    }else{
+                        answer = prompt(name + ', YOU NEED TO CHOOSE EITHER 1 OR 2. THERE IS '+ marbleCount + ' MARBLE LEFT. PLS TRY AGAIN!')
+                    }
+                }else{
+                    if(marbleCount != 1){
+                        answer = prompt(name + ', there are ' + marbleCount + ' marbles remaining. Choose how many marbles you want to take below. You may choose either 1 or 2.')
+                        answered = true;
+                    }else{
+                        answer = prompt(name + ', there is ' + marbleCount + ' marble remaining. Choose how many marbles you want to take below. You may choose either 1 or 2.')
+                        answered = true;
+                    }
+                }
+                answer = parseInt(answer)
+                console.log()
+            }
+        }else{
+            if(marbleCount === 1){
+                while(answer != 1){
+                    if(answered){
+                        if(marbleCount != 1){
+                            answer = prompt(name + ', YOU NEED TO CHOOSE UPTO HALF. THERE ARE '+ marbleCount + ' MARBLES LEFT. PLS TRY AGAIN!')
+                        }else{
+                            answer = prompt(name + ', YOU NEED TO CHOOSE UPTO HALF. THERE IS '+ marbleCount + ' MARBLE LEFT. PLS TRY AGAIN!')
+                        }
+                    }else{
+                        if(marbleCount != 1){
+                            answer = prompt(name + ', there are ' + marbleCount + ' marbles remaining. Choose how many marbles you want to take below. You may choose upto half.')
+                            answered = true;
+                        }else{
+                            answer = prompt(name + ', there is ' + marbleCount + ' marble remaining. Choose how many marbles you want to take below. You may choose upto half.')
+                            answered = true;
+                        }
+                    }
+                    answer = parseInt(answer)
+                    console.log()
+                }
+            }else{
+                while(answer > marbleCount/2 || answer === undefined || answer === 0){
+                    if(answered){
+                        if(marbleCount != 1){
+                            answer = prompt(name + ', YOU NEED TO CHOOSE UPTO HALF. THERE ARE '+ marbleCount + ' MARBLES LEFT. PLS TRY AGAIN!')
+                        }else{
+                            answer = prompt(name + ', YOU NEED TO CHOOSE UPTO HALF. THERE IS '+ marbleCount + ' MARBLE LEFT. PLS TRY AGAIN!')
+                        }
+                    }else{
+                        if(marbleCount != 1){
+                            answer = prompt(name + ', there are ' + marbleCount + ' marbles remaining. Choose how many marbles you want to take below. You may choose upto half.')
+                            answered = true;
+                        }else{
+                            answer = prompt(name + ', there is ' + marbleCount + ' marble remaining. Choose how many marbles you want to take below. You may choose upto half.')
+                            answered = true;
+                        }
+                    }
+                    answer = parseInt(answer)
+                    console.log()
+                }
+            }
+        }
+
+        marbleCount -= answer
+        alert('You took ' + answer + ' marbles. There are ' + marbleCount + ' marbles left.')
+        answered = false
+        turn = 2;
+        if (marbleCount < 1){
+            alert('You Won!')
+            playerScore++;
+            localStorage.setItem('playerScore',playerScore)
+            gameOver = true;
+        }
+}
+
+
+function computerTurn(){
+    if(amount == 1){
+        let marblesTaken = Math.floor(Math.random()*2 ) + 1
+        if(comFirst) alert('There are ' + marbleCount + ' marbles in the pile.')
+        comFirst = false
+        marbleCount -= marblesTaken
+        alert('The computer took ' + marblesTaken + ' marbles.')
+        turn = 1;
+        if (marbleCount < 1){
+            alert('Computer Won! You Lost.')
+            compScore++;
+            localStorage.setItem('compScore',compScore)
+            gameOver = true;
+        }
+    }else{
+        if(mode == 'easy'){
+            let possibleMarbles = marbleCount/2
+            let marblesTaken = Math.floor(Math.random()* possibleMarbles) + 1
+            if(comFirst) alert('There are ' + marbleCount + ' marbles in the pile.')
+            comFirst = false
+            marbleCount -= marblesTaken
+            alert('The computer took ' + marblesTaken + ' marbles.')
+            turn = 1;
+            if (marbleCount < 1){
+                alert('Computer Won! You Lost.')
+                compScore++;
+                localStorage.setItem('compScore',compScore)
+                gameOver = true;
+            }
+        }else if(mode == 'hard'){
+            let marblesTaken = marbleCount
+            let counter = 1;
+            if(comFirst) alert('There are ' + marbleCount + ' marbles in the pile.')
+            comFirst = false
+            while(2**counter < marbleCount){
+                counter++
+            }
+            counter--;
+            if(marbleCount !== 1){
+                marblesTaken -= 2**counter
+                marbleCount = 2**counter
+            }else{
+                marblesTaken = 1;
+                marbleCount = 0;
+            }
+    
+            alert('The computer took ' + marblesTaken + ' marbles.')
+            turn = 1;
+            if (marbleCount < 1){
+                alert('Computer Won! You Lost.')
+                compScore++;
+                localStorage.setItem('compScore',compScore)
+                gameOver = true;
+            }
+        }
+    }
+}
